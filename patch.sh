21712d8a32b05935995fb13788163dd11a0d331e
Daniel Kostecki <dkosteck@redhat.com>
2025-06-13 16:24:06 -0400

    Repo CMP, Render CMP, build files, and patch

---
+++ patch.sh
@@ -0,0 +1,79 @@
+kubectl patch deployment argocd-repo-server -n argocd --type json -p='[
+   {
+    "op": "add",
+    "path": "/spec/template/spec/containers/-",
+    "value": {
+      "name": "kpt-repo-plugin",
+      "image": "dkostecki/kpt-repo-plugin:v1",
+      "command": ["/var/run/argocd/argocd-cmp-server"],
+      "securityContext": {
+          "runAsNonRoot": true,
+          "runAsUser": 999
+      },
+      "volumeMounts": [
+        {
+           "name": "var-files",
+           "mountPath": "/var/run/argocd"
+        },
+        {
+           "name": "cmp-tmp",
+           "mountPath": "/tmp"
+        },
+        {
+           "name": "plugins",
+           "mountPath": "/home/argocd/cmp-server/plugins"
+        }
+      ]
+    }
+  },
+  {
+    "op": "add",
+    "path": "/spec/template/spec/containers/-",
+    "value": {
+      "name": "kpt-render-plugin",
+      "image": "dkostecki/kpt-render-plugin:v1",
+      "command": ["/var/run/argocd/argocd-cmp-server"],
+      "securityContext": {
+          "runAsNonRoot": true,
+          "runAsUser": 999
+      },
+      "volumeMounts": [
+        {
+           "name": "var-files",
+           "mountPath": "/var/run/argocd"
+        },
+        {
+           "name": "cmp-tmp",
+           "mountPath": "/tmp"
+        },
+        {
+           "name": "plugins",
+           "mountPath": "/home/argocd/cmp-server/plugins"
+        }
+      ]
+    }
+  },
+  {
+    "op": "add",
+    "path": "/spec/template/spec/volumes/-",
+    "value": {
+       "name": "cmp-tmp",
+       "emptyDir": {}
+    }
+  },
+  {
+    "op": "add",
+    "path": "/spec/template/spec/volumes/-",
+    "value": {
+       "name": "var-run-argocd",
+       "emptyDir": {}
+    }
+  }
+]'
+
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+    echo "patched"
+fi
+
